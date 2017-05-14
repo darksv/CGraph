ca646afbc13474193651e3c63479bc359cf735ad
darksv <darek969-12@o2.pl>
2017-05-15 00:18:29 +0200

    icons

--- CGraph/Properties/AssemblyInfo.cs
+++ CGraph/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CGraph")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Graph Generator")]
+[assembly: AssemblyDescription("Generator grafów")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CGraph")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCompany("ATH")]
+[assembly: AssemblyProduct("Graph Generator")]
+[assembly: AssemblyCopyright("Copyright © ATH 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,7 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
+[assembly: NeutralResourcesLanguage("pl-PL")]
+
