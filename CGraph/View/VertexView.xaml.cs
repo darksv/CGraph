d79a586f357e1177d4f4b23b8f8af132e7fb2d57
Niedoba Dariusz <darek969-12@o2.pl>
2017-11-02 16:12:38 +0100

    Add color property for vertex.

--- CGraph/View/VertexView.xaml.cs
+++ CGraph/View/VertexView.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace CGraph.View
 {
@@ -31,6 +33,66 @@ namespace CGraph.View
         {
             get => (string) GetValue(LabelProperty);
             set => SetValue(LabelProperty, value);
+        }
+
+        public static readonly DependencyProperty ColorProperty
+            = DependencyProperty.Register(
+                nameof(Color),
+                typeof(Color),
+                typeof(VertexView),
+                new PropertyMetadata(Colors.White, PropertyChangedCallback)
+            );
+
+        private static void PropertyChangedCallback(DependencyObject sender, DependencyPropertyChangedEventArgs args)
+        {
+            var self = (VertexView) sender;
+            self.BackgroundBrush = new SolidColorBrush(self.Color);
+
+            var color = self.Color;
+            var intensity = color.R * 0.299 + color.G * 0.587 + color.B * 0.114;
+            var textColor = intensity < 0.5 ? Colors.White : Colors.Black;
+
+            self.TextBrush = new SolidColorBrush(textColor);
+        }
+
+        public Color Color
+        {
+            get => (Color) GetValue(ColorProperty);
+            set => SetValue(ColorProperty, value);
+        }
+
+        private static readonly DependencyPropertyKey TextBrushPropertyKey
+            = DependencyProperty.RegisterReadOnly(
+                nameof(TextBrush),
+                typeof(Brush),
+                typeof(VertexView),
+                new PropertyMetadata(Brushes.Black)
+            );
+
+        public static readonly DependencyProperty TextBrushProperty
+            = TextBrushPropertyKey.DependencyProperty;
+
+        public Brush TextBrush
+        {
+            get => (Brush)GetValue(TextBrushProperty);
+            private set => SetValue(TextBrushPropertyKey, value);
+        }
+
+        private static readonly DependencyPropertyKey BackgroundBrushPropertyKey
+            = DependencyProperty.RegisterReadOnly(
+                nameof(BackgroundBrush),
+                typeof(Brush),
+                typeof(VertexView),
+                new PropertyMetadata(Brushes.White)
+            );
+
+        public static readonly DependencyProperty BackgroundBrushProperty
+            = BackgroundBrushPropertyKey.DependencyProperty;
+
+        public Brush BackgroundBrush
+        {
+            get => (Brush)GetValue(BackgroundBrushProperty);
+            private set => SetValue(BackgroundBrushPropertyKey, value);
         }
 
         public VertexView()
